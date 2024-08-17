6d17338585c171ab6c777eeefb51a8d0ca6bc802
Danylo Kondratiev <knightpp@proton.me>
2024-08-17 13:24:19 +0300

    Lower elixir version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule HikkaBackup.MixProject do
     [
       app: :hikka_backup,
       version: "0.1.0",
-      elixir: "~> 1.17",
+      elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       escript: [main_module: HikkaBackup]
